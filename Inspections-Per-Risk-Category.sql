af6158d462e77e6373f8275421d178d3ba4a8793
Jason Yang <99283954+ygsjason@users.noreply.github.com>
2022-05-07 15:09:49 -0500

    Update Inspections-Per-Risk-Category.sql

--- Inspections-Per-Risk-Category.sql
+++ Inspections-Per-Risk-Category.sql
@@ -1,3 +1,4 @@
+--method_1 using case when to substitue the Null
 select
   case
     when risk_category isnull then 'No Risk'
@@ -7,3 +8,10 @@ select
 from sf_restaurant_health_violations
 group by risk_category
 order by n_isp desc
+
+--method_2 using coalesce to substitue the Null
+select coalesce(risk_category, 'No Risk'), count(inspection_id) as n_isp
+from sf_restaurant_health_violations
+group by risk_category
+order by n_isp desc
+
